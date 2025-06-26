aba40c5f4acac6737e3f345806e7f743485b421a
Muna Berhe <m.berhe19@gmail.com>
2025-06-26 07:12:34 -0500

    Update align_only.sh

--- scripts/align_only.sh
+++ scripts/align_only.sh
@@ -6,7 +6,7 @@
 
 set -e
 
-echo "🔹 Aligning PAIRED-END SRR2131556..."
+echo " Aligning PAIRED-END SRR2131556..."
 STAR --runThreadN 4 \
  --genomeDir ref/star_index \
  --readFilesIn data/trimmed/SRR2131556_R1.trimmed.fastq.gz data/trimmed/SRR2131556_R2.trimmed.fastq.gz \
@@ -14,7 +14,7 @@ STAR --runThreadN 4 \
  --outFileNamePrefix results/star/SRR2131556_ \
  --outSAMtype BAM SortedByCoordinate
 
-echo "🔹 Aligning SINGLE-END SRR2131557..."
+echo " Aligning SINGLE-END SRR2131557..."
 STAR --runThreadN 4 \
  --genomeDir ref/star_index \
  --readFilesIn data/trimmed/SRR2131557.trimmed.fastq.gz \
@@ -22,5 +22,5 @@ STAR --runThreadN 4 \
  --outFileNamePrefix results/star/SRR2131557_ \
  --outSAMtype BAM SortedByCoordinate
 
-echo "✅ DONE: Alignment finished!"
+echo " DONE: Alignment finished!"
 
